d33bf64859557e7f7bbf774697aa07be0f8fbe3d
lomalo890 <george2066@yandex.ru>
2025-02-27 15:23:28 +0300

    feat: methods for DB

--- telegrambotcalendar/henlers.py
+++ telegrambotcalendar/henlers.py
@@ -1,17 +1,35 @@
-def create_note(note_text, note_name):
-    pass
+import psycopg2 as pg
+from telegrambotcalendar.secrets import HOST, DATABASE, USER, PASSWORD
+
+connection = pg.connect(
+    host=HOST,
+    database=DATABASE,
+    user=USER,
+    password=PASSWORD
+)
+
+create_events = '''
+CREATE TABLE events (
+    id serial PRIMARY KEY,
+    user_id INT NOT NULL,
+    name text NOT NULL,
+    date date NOT NULL,
+    time time NOT NULL
+);
+'''
+
+cursor = connection.cursor()
+
+cursor.execute("select * from information_schema.tables where table_name=\'events\'")
+
+if not bool(cursor.rowcount):
+    cursor.execute(create_events)
+    connection.commit()
+    print("Таблица успешно создана в PostgreSQL")
 
 
-def read_note(note_name):
-    return None
+insert = lambda user_id, event_name, event_text, event_date, event_time: f'insert into events (user_id, name, text, date, time) value(\'{user_id}\', \'{event_name}\', \'{event_text}\', \'{event_date}\', \'{event_time}\')'
 
+def create_event(user_id, event_name, event_text):
+    cursor.execute()
 
-def update_note(note_name, note_text):
-    return None
-
-
-def delete_note(note_name):
-    return None
-
-def read_all_notes(note_name):
-    pass
---
+++ telegrambotcalendar/models.py
@@ -0,0 +1,22 @@
+class Calendar:
+    def __init__(self):
+        self.events = {}
+
+    # Создать метод create_event
+    def create_event(self, event_name, event_date, event_time, event_details):
+        pass
+
+    def read_event(self, event_id):
+        return self.events[event_id]
+
+    def update_event(self, event_id, event_name, event_date, event_time, event_details):
+        pass
+
+    def delete_event(self, event_id):
+        pass
+
+
+
+
+
+
